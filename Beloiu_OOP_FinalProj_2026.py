5c24ec2ab63ff5508dcd2411e9c03e1a46928304
arianabeloiu <160290843+arianabeloiu@users.noreply.github.com>
2026-03-26 19:17:35 -0700

    Fully Commented Code

--- Beloiu_OOP_FinalProj_2026.py
+++ Beloiu_OOP_FinalProj_2026.py
@@ -89,89 +89,159 @@ aa_mol_weights={'A':89.09,'C':121.15,'D':133.1,'E':147.13,'F':165.19,
                 'S':105.09,'T':119.12,'V':117.15,'W':204.23,'X':0,'Y':181.19}
 
 
-class Seq:
+class Seq: 
+    """  
+    This class will allow you to extract information about the sequence, 
+    create k-mers, and print out the information in a fasta format 
+    
+    """
 
-    def __init__(self,sequence,gene,species):
-        self.sequence=sequence.upper().strip()
-        self.gene=gene
-        self.species=species
-        self.kmers = [] 
+    def __init__(self,sequence,gene,species): 
+        """  
+        initalize Seq with sequence, gene, and species
+        """
+        self.sequence=sequence.upper().strip() #assign sequence input to sequence, uppercase the sequence + strips the beginning & end of the sequence 
+        self.gene=gene #assign gene input to gene 
+        self.species=species #assign species input to species 
+        self.kmers = [] #assign kmers to an empty list 
 
-    def __str__(self):
-        return self.sequence
+    def __str__(self): 
+        """
+        overload the print function so that it prints the sequence 
+        """
+        return self.sequence #prints the sequence 
 
-    def print_record(self):
-        print(self.species + " " + self.gene + ": " + self.sequence)
+    def print_record(self): 
+        """
+        print information about the Sequence 
+        """
+        print(self.species + " " + self.gene + ": " + self.sequence) #prints the species, gene and sequence 
 
-    def make_kmers(self, k=3):
-        for i in range(len(self.sequence)-k+1):
-            kmer=self.sequence[i:i+k]
-            self.kmers.append(kmer)  
+    def make_kmers(self, k=3): 
+        """
+        find all the overlapping k-mers in the sequence, with a default k-mer size = 3. 
+        """
+        for i in range(len(self.sequence)-k+1): #iterate through the sequence, extracting k-mers of length k 
+            kmer=self.sequence[i:i+k] #slice the sequence up until the length of the k-mer size 
+            self.kmers.append(kmer) #append to an empty list
 
     def fasta(self):
+        """ 
+        print out information in fasta format
+        """
         return ">" + str(self.species) + "|" + str(self.gene) + "\n" + str(self.sequence)
     
 class DNA(Seq):
 
-    def __init__(self,sequence,gene,species,geneid,**kwargs):
-        super().__init__(sequence,gene,species)
-        self.sequence = re.sub('[^ATGCU]', 'N', self.sequence)
-        self.geneid=geneid
+    """ 
+    
+    This class will inherit all the functions of the Seq class + 
+    conduct GC count, reverse complement the sequence, and find the first six frames
+    of the forward and complementary sequence. 
+
+    """
+
+    def __init__(self,sequence,gene,species,geneid,**kwargs): 
+        """ 
+        initalize DNA with the same parameters as the Seq class + geneid
+        """
+        super().__init__(sequence,gene,species) #use super to inherit the previous methods from Seq class 
+        self.sequence = re.sub('[^ATGCU]', 'N', self.sequence) #assign sequence input to sequence + use regex package to substitute any character that's not ATGCU as N
+        self.geneid=geneid #assign geneid input to gene id
  
-    def analysis(self):
-        gc=len(re.findall('G',self.sequence) + re.findall('C',self.sequence))
-        return gc
+    def analysis(self): 
+        """ 
+        find the GC counts 
+        """
+        gc=len(re.findall('G',self.sequence) + re.findall('C',self.sequence)) #use regex package to get the sum of G's and C's in the sequence 
+        return gc #print the result
 
-    def print_info(self):
-        print(self.geneid + " " + self.species + " " + self.gene + ": " + self.sequence)
+    def print_info(self): 
+        """ 
+        print information about the DNA 
+        """
+        print(self.geneid + " " + self.species + " " + self.gene + ": " + self.sequence) #prints out the geneid, species name, gene, and sequence 
 
-    def reverse_complement(self): 
-        complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A','N':'N'}
-        return "".join(complement.get(base,'N') for base in reversed(self.sequence))
+    def reverse_complement(self):  
+        """ 
+        reverse complement the sequence 
+        """
+        complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A','N':'N'} #set up a dictionary of the corresponding base-pairs
+        return "".join(complement.get(base,'N') for base in reversed(self.sequence)) #uses the dictionary to get the reverse complement of the sequence 
 
     def six_frames(self): 
-        frames = []
-        reverse_complement = self.reverse_complement()
-        for i in range(3):
-            frames.append(self.sequence[i:])
-        for i in range(3):
+        """
+        finds 6 frames of the sequence (3 from the forward sequence, 3 from the reverse complement 
+        """
+        frames = [] #create an empty list called frames 
+        reverse_complement = self.reverse_complement() #assigns reverse complement input to be the reverse complement of the sequence 
+
+        #get the first 3 frames from the forward sequence 
+        for i in range(3): 
+            frames.append(self.sequence[i:]) 
+
+        #get the first 3 frames from the reverse complement
+        for i in range(3): 
             frames.append(reverse_complement[i:]) 
-        return frames
+        return frames #print all the 6 frames 
 
 
 class RNA(DNA):
-
-    def __init__(self,sequence,gene,species,geneid,**kwargs):
-        super().__init__(sequence,gene,species)
+    """ 
+    This class will inherit all the functions of the DNA class +
+    get the codons and translate the sequence 
+    """
+    def __init__(self,sequence,gene,species,geneid,**kwargs): 
+        """
+        initialize RNA class with the same parameters as the DNA class 
+        """
+        super().__init__(sequence,gene,species) #use super to inherit the previous methods from the DNA class 
+        self.sequence = re.sub('T','U',self.sequence) #replace every T base with a U 
+        self.codons = [] #assign codons to an empty list 
         
-        
-    def make_codons(self): 
-        for i in range(0,len(self.sequence),3):
-            codon = self.sequence[i:i+3]
-            self.codons.append(codon)
+    def make_codons(self):  
+        """
+        get the codons from the sequence 
+        """
+        for i in range(0,len(self.sequence),3): #iterate through the sequence skipping by 3s 
+            codon = self.sequence[i:i+3] #splice the sequence by every 3 bases 
+            self.codons.append(codon) #add the codons to the empty codons list 
  
-    def translate(self):
-        proteins = ""
-        for codon in self.codons:
+    def translate(self): 
+        """
+        translate the RNA sequence into amino acids 
+        """
+        protein = "" #assign proteins to an empty string 
+        for codon in self.codons: #iterate through each codons in the codons list  
+            #use a try & except to translate the codons into amino acids 
             try:
-                aa= standard_code[codon]
+                aa= standard_code[codon] #use the dictionary to get the corresponding amino acid 
             except:
-                aa = "X"
-            proteins += aa
-        return proteins
+                aa = "X" #if the pattern doesn't exist in the dictionary, denote it as X 
+            protein += aa #add the amino acid to the protein string 
+        return protein #print out the protein sequence 
 
 class Protein(Seq):
+    """
+    This class will inherit all the functions of the Seq class +
+    calculate the total hydrophobicity and total molecular weight of the protein sequence
+    """
+    def __init__(self,sequence,gene,species,geneid,**kwargs): 
+        """
+        initialize the Protein class with the same parameters as the Seq class
+        """
+        super().__init__(sequence,gene,species) #use super() to inherit the previous methods from the Seq class 
+        self.sequence = re.sub('[^0-9a-zA-Z]+', 'X', self.sequence) #use the regex package to substitute any non LETTER characters in the sequence into an 'X'
 
-    def __init__(self,sequence,gene,species,geneid,**kwargs):
-        super().__init__(sequence,gene,species)
-        self.sequence = re.sub('[^0-9a-zA-Z]+', 'X', self.sequence)
-
-    def total_hydro(self): 
-        hydro_score = sum(kyte_doolittle.get(aa, 0) for aa in self.sequence)
-        return hydro_score
+    def total_hydro(self):  
+        """ 
+        calculate the total hydrophobicity score
+        """
+        hydro_score = sum(kyte_doolittle.get(aa, 0) for aa in self.sequence) #sum the hydrophobicity values for all amino acids in the sequence
+        return hydro_score 
 
     def mol_weight(self):
-        total_mol_weight = sum(aa_mol_weights.get(aa, 0) for aa in self.sequence)
+        total_mol_weight = sum(aa_mol_weights.get(aa, 0) for aa in self.sequence) #sum the total molecular weight 
         return total_mol_weight
         
 
